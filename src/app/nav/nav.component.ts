27063d7786ec72174026986626da6e17ba9adddf
Rahmatullo Kholov <rahajustone>
2020-05-29 11:50:16 -0500

    Add  module 5

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -19,7 +19,7 @@ export class NavComponent implements OnInit {
     this.authService.login(this.model).subscribe(next => {
       console.log('Succesfully loged in.');
     }, error => {
-      console.log('Failed to loged in.' + error);
+      console.log(error);
     });
   }
 
